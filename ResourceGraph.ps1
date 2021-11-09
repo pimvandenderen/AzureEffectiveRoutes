82d1ccdde7653c3557fdc1f33a79ba957ee0417f
Pim van Denderen <pvandenderen@microsoft.com>
2021-11-09 08:32:24 -0500

    ResourceGraph

---
+++ ResourceGraph.ps1
@@ -0,0 +1,15 @@
+﻿# Install the Resource Graph module from PowerShell Gallery
+Install-Module -Name Az.ResourceGraph
+
+$graphquery = "Resources
+| where type =~ 'microsoft.network/networkinterfaces' 
+| where isnotempty(properties.virtualMachine)
+| project id, ipConfigurations = properties.ipConfigurations
+| mvexpand ipConfigurations
+| project id, subnetId = tostring(ipConfigurations.properties.subnet.id)
+| parse kind=regex subnetId with '/virtualNetworks/' virtualNetwork '/subnets/' subnet 
+| project id, virtualNetwork, subnet"
+
+
+
+$output = Search-AzGraph $graphquery
